fdb8502d16ccfcf91744d553cc9481618b5ece26
Nguyen Van Bang <61932751+nvbang14061992@users.noreply.github.com>
2025-09-21 18:09:49 +0700

    init new api module for authentication service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ConfigModule } from './modules/modules-system/config/config.module';
 import { PrismaModule } from './modules/modules-system/prisma/prisma.module';
 import { RoomModule } from './modules/modules-api/room/room.module';
+import { AuthModule } from './modules/modules-api/auth/auth.module';
 
 @Module({
-  imports: [ConfigModule, PrismaModule, RoomModule],
+  imports: [ConfigModule, PrismaModule, RoomModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
