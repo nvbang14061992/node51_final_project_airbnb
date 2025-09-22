94b32f406e016fb4cb42b1d3459ae2501e873c76
Nguyen Van Bang <61932751+nvbang14061992@users.noreply.github.com>
2025-09-22 20:10:57 +0700

    swagger add

--- src/modules/modules-api/auth/dto/login.dto.ts
+++ src/modules/modules-api/auth/dto/login.dto.ts
@@ -1,15 +1,12 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, isString } from 'class-validator';
 
 export class LoginDto {
   @IsNotEmpty()
   @IsString()
   @IsEmail()
-  @ApiProperty({ example: 'user@abc.com' })
   email: string;
 
   @IsString()
   @IsNotEmpty()
-  @ApiProperty({ example: 'pass123' })
   password: string;
 }
