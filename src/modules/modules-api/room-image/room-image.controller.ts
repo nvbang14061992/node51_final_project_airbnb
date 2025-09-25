9d54241c71b05b22e1495b61aed0a577f88cebd4
Nguyen Van Bang <61932751+nvbang14061992@users.noreply.github.com>
2025-09-25 23:29:37 +0700

    - add ma_nguoi_tao to database - table HinhAnh_phong - add finAll method to room-image module

--- src/modules/modules-api/room-image/room-image.controller.ts
+++ src/modules/modules-api/room-image/room-image.controller.ts
@@ -11,37 +11,36 @@ import { RoomImageService } from './room-image.service';
 import { CreateRoomImageDto } from './dto/create-room-image.dto';
 import { UpdateRoomImageDto } from './dto/update-room-image.dto';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { MessageResponse } from 'src/common/decorators/message-response.decorator';
+import { CurrentUser } from 'src/common/decorators/user.decorator';
+import type { Users } from 'generated/prisma';
 
-@Controller('room-image')
+@Controller('hinh-anh-phong-thue')
 @ApiBearerAuth()
 export class RoomImageController {
   constructor(private readonly roomImageService: RoomImageService) {}
 
   @Post()
+  @MessageResponse('Upload a room image successfully!')
   create(@Body() createRoomImageDto: CreateRoomImageDto) {
     return this.roomImageService.create(createRoomImageDto);
   }
 
   @Get()
+  @MessageResponse('Get all room images successfully!')
   findAll() {
     return this.roomImageService.findAll();
   }
 
   @Get(':id')
+  @MessageResponse('Get a room image successfully!')
   findOne(@Param('id') id: string) {
     return this.roomImageService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateRoomImageDto: UpdateRoomImageDto,
-  ) {
-    return this.roomImageService.update(+id, updateRoomImageDto);
-  }
-
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.roomImageService.remove(+id);
+  @MessageResponse('Delete a room image successfully!')
+  remove(@Param('id') id: string, @CurrentUser() user: Users) {
+    return this.roomImageService.remove(+id, user);
   }
 }
--- src/modules/modules-api/room-image/room-image.service.ts
+++ src/modules/modules-api/room-image/room-image.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateRoomImageDto } from './dto/create-room-image.dto';
 import { UpdateRoomImageDto } from './dto/update-room-image.dto';
+import { PrismaService } from 'src/modules/modules-system/prisma/prisma.service';
+import { Users } from 'generated/prisma';
 
 @Injectable()
 export class RoomImageService {
-  create(createRoomImageDto: CreateRoomImageDto) {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findRoomImageExisting(id: number) {
+    const locationExist = await this.prisma.viTri.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!locationExist) throw new BadRequestException('Not found room!!!');
+
+    return locationExist;
+  }
+
+  async create(createRoomImageDto: CreateRoomImageDto) {
     return 'This action adds a new roomImage';
   }
 
-  findAll() {
-    return `This action returns all roomImage`;
+  async findAll() {
+    const rooms = await this.prisma.hinhAnh_Phong.findMany();
+    return rooms;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} roomImage`;
+  async findOne(id: number) {
+    const existingImage = await this.findRoomImageExisting(id);
+    return existingImage;
   }
 
-  update(id: number, updateRoomImageDto: UpdateRoomImageDto) {
-    return `This action updates a #${id} roomImage`;
-  }
+  async remove(id: number, user: Users) {
+    const existingImage = await this.findRoomImageExisting(id);
 
-  remove(id: number) {
-    return `This action removes a #${id} roomImage`;
+    return true;
   }
 }
