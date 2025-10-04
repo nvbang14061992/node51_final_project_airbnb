65ef2b240afbc6b6afcdfee961865b4101f0a13f
Nguyen Van Bang <61932751+nvbang14061992@users.noreply.github.com>
2025-10-04 11:02:44 +0700

    - add specific rate limiters for specific requests / duration - add customize tracker for not tracking IP but tracking device-id

--- src/modules/modules-system/rate-limiting/throttle.module.ts
+++ src/modules/modules-system/rate-limiting/throttle.module.ts
@@ -1,6 +1,11 @@
-import { Global, Module } from '@nestjs/common';
+import { ExecutionContext, Global, Logger, Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
-import { hours, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import {
+  hours,
+  minutes,
+  ThrottlerGuard,
+  ThrottlerModule,
+} from '@nestjs/throttler';
 
 @Global()
 @Module({
@@ -8,10 +13,32 @@ import { hours, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
     ThrottlerModule.forRoot({
       throttlers: [
         {
+          name: 'short',
+          ttl: minutes(1),
+          limit: 30,
+          blockDuration: hours(2),
+        },
+        {
+          name: 'medium',
+          ttl: minutes(10),
+          limit: 100,
+          blockDuration: hours(2),
+        },
+        {
+          name: 'long',
           ttl: hours(2),
-          limit: 200,
+          limit: 300,
+          blockDuration: hours(2),
         },
       ],
+      getTracker: (req: Record<string, any>, context: ExecutionContext) => {
+        // override
+        // track on company id instead of ip, because one router's IP can be use by multiple company or user in public place like hotel, park
+        const companyId = req.headers['x-device-id'];
+        const logger = new Logger('IP-tracker');
+        logger.log(`Company Id: ${companyId}`);
+        return companyId;
+      },
     }),
   ],
   providers: [
